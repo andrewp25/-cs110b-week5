c9a5dd892476ccde8b135c889e8a1b1552816b3e
andrewp25 <42699826+andrewp25@users.noreply.github.com>
2018-09-29 12:47:44 -0700

    Add files via upload

---
+++ midterm_1_problem4.cc
@@ -0,0 +1,31 @@
+/*
+
+An Employee has a name, and ID number, title, department hourly rate and number
+of hours worked per week. Write a class for employee, and print using functions,
+employee details with their weekly wages in the descending order.
+
+*/
+
+#include <iostream>
+#include <String>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+
+  class EmployeeWages
+  {
+    public:
+      String employeeName;
+      int employeeID;
+      String employeeTitle;
+      String department;
+      double hourlyWage;
+      int hoursWorked;
+
+      // Ran out of time.
+  }
+  return 0;
+}
